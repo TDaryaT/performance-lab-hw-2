f411ab519c4cc40894ffb35c8efe726e878b73f4
Darya Tlepbergenova <tdaryat@gmail.com>
2020-12-17 23:57:44 +0300

    first init commit

---
+++ src/main/java/tlepbergenova/Main.java
@@ -0,0 +1,11 @@
+package tlepbergenova;
+
+//import ru.pflb.mq.dummy.implementation.SessionImpl;
+//import ru.pflb.mq.dummy.interfaces.Session;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hi maven!");
+        //Session session = new SessionImpl();
+    }
+}
